1781958be9ff963a9a8fadce23d9a825c259d868
Zachary Groves <zgroves19@gmail.com>
2018-05-26 21:19:32 -0400

    should be good now

--- m1-w1d3-expressions-and-control-flow-exercises/src/main/java/com/techelevator/Exercises.java
+++ m1-w1d3-expressions-and-control-flow-exercises/src/main/java/com/techelevator/Exercises.java
@@ -34,12 +34,6 @@ public class Exercises {
 	 sumDouble(2, 2) → 8
 	 */
 	public int sumDouble(int a, int b) {
-<<<<<<< HEAD
-		if (a==b) {
-			return (a + b) * 2;
-		}
-		return a + b;
-=======
 		int sum = 0;
 		if (a == b) {
 			sum = 2 * (a+b);
@@ -48,7 +42,6 @@ public class Exercises {
 			sum = a+b;
 		}
 		return sum;
->>>>>>> caf3b9c65a60abb03d58dc393b70f675b070723f
 	}
 
 	/*
