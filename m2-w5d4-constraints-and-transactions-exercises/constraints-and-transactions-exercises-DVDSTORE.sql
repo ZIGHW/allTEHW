2c62e16d8242a700fd108b19108b05eb2feddc02
Zachary Groves <zgroves19@gmail.com>
2018-06-21 13:45:03 -0400

    finished m2-w5-d4 exercises

--- m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-DVDSTORE.sql
+++ m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-DVDSTORE.sql
@@ -1,42 +1,78 @@
 -- Write queries to return the following:
 -- The following changes are applied to the "pagila" database.**
-
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+INSERT INTO actor (actor_id, first_name, last_name)
+VALUES (10000, 'Hampton', 'Avenue')
 
+INSERT INTO actor (actor_id, first_name, last_name)
+VALUES (10001, 'Lisa', 'Byway')
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in 
 -- ancient Greece", to the film table. The movie was released in 2008 in English. 
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special 
 -- features, the film speaks for itself, and doesn't need any gimmicks.	
+INSERT INTO film VALUES (10000, 'Euclidian PI', 'The epic story of Euclid as a pizza delivery boy in 
+-- ancient Greece', 2008, 1, null, 5.00, .05, 198, 200, 'NC-17')
 
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly 
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+INSERT INTO film_actor (actor_id, film_id)
+VALUES (10001, 10000)
 
+INSERT INTO film_actor (actor_id, film_id)
+VALUES (10000, 10000)
 -- 4. Add Mathmagical to the category table.
+INSERT INTO category VALUES (10000, 'Mathmagical')
+
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI", 
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+INSERT INTO film_category VALUES (10000  , 10000)
+INSERT INTO film_category VALUES (274  , 10000)
+INSERT INTO film_category VALUES (494  , 10000)
+INSERT INTO film_category VALUES (714  , 10000)
+INSERT INTO film_category VALUES (996  , 10000)
+
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films 
 -- accordingly.
 -- (5 rows affected)
+UPDATE film
+SET rating = 'G'
+WHERE film.film_id IN (SELECT film.film_id FROM film_category WHERE film_category.category_id = 10000)
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+INSERT INTO inventory VALUES (4582, 10000, 1)
+INSERT INTO inventory VALUES (4583, 10000, 2)
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film, 
 -- "Euclidean PI". The film has been seized from all stores, and needs to be 
 -- deleted from the film table. Delete "Euclidean PI" from the film table. 
 -- (Did it succeed? Why?)
+DELETE FROM film 
+WHERE title = 'Euclidian PI'
+--can't delete becasue it violates the foreign key constraint in film_actor table
 
 -- 9. Delete Mathmagical from the category table. 
 -- (Did it succeed? Why?)
+DELETE FROM category 
+WHERE category_id = 10000
+--can't delete becasue it violates the foreign key constraint in film_category table
 
 -- 10. Delete all links to Mathmagical in the film_category tale. 
 -- (Did it succeed? Why?)
+DELETE FROM film_category
+WHERE category_id = 10000
+--it worked because there were no contrainsts placed on that table as those being foreign
+--for other tables
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI". 
 -- (Did either deletes succeed? Why?)
 
--- 12. Check database metadata to determine all constraints of the film id, and 
--- describe any remaining adjustments needed before the film "Euclidean PI" can 
--- be removed from the film table.
+DELETE FROM category 
+WHERE category_id = 10000
+--yes because mathmagical is no longer linked to the category_film table as a foreign key
+
+DELETE FROM film 
+WHERE title = 'Euclidian PI'
+--no because Euclidian PI is still linked to the foreign key film_actor
--- m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-WORLD.sql
+++ m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-WORLD.sql
@@ -4,31 +4,51 @@
 -- 1. Add Superman's hometown, Smallville, Kansas to the city table. The 
 -- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
 -- Wikipedia.)
+INSERT INTO city (ID, name, countrycode, district, population) 
+VALUES (100000, 'Smallville', 'USA', 'Kansas', 45001)
+
 
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
-
+INSERT INTO countrylanguage (countrycode, language, isOfficial, percentage)
+VALUES ('USA', 'Kryptonese', false, .0001 )
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
 -- the appropriate record accordingly.
-
+UPDATE countrylanguage 
+SET language = 'Krypto-babble'
+WHERE language = 'Kryptonese' AND countrycode = 'USA'
 -- 4. Set the US captial to Smallville, Kansas in the country table.
-
+UPDATE country
+SET capital = 100000
+WHERE country.code = 'USA'
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
-
+DELETE FROM city
+WHERE id = 100000;
+no because city holds the foreign key for capital in country
 -- 6. Return the US captial to Washington.
-
+UPDATE country
+SET capital = 3813
+WHERE country.code = 'USA'
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
-
+DELETE FROM city
+WHERE id = 100000
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
+UPDATE countrylanguage
+SET isofficial = NOT isofficial
+WHERE countrylanguage.countrycode IN (SELECT country.code FROM country WHERE country.indepyear > 1800 AND country.indepyear < 1972)
 
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
-
+UPDATE city
+SET population = population / 1000
 -- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
 -- meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+UPDATE country
+SET surfacearea = surfacearea * (2.59 * 10000000)
+WHERE country.code IN (SELECT countrylanguage.countrycode FROM countrylanguage WHERE countrylanguage.percentage > 20 AND language = 'French')
